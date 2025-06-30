aba9b631e882501e90621832edbc2aaed045ae20
Bill Nagbe <BillyNagbe@gmail.com>
2025-06-30 17:54:40 -0500

    Error css/js not loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,20 @@ let input, btn, taskList;
 input = document.getElementById("input-text");
 
 taskList = document.getElementById("task-list ");
+
+
+function addTask() {
+    if(input.value === " ")  alert("Put something in the box");
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        taskList.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+
+    input.value = " ";
+}
+
+addTask();
